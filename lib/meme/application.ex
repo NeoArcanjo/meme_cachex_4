ab3bf7d230bcfc363a5e19f7c1e6c268318e8686
Rafael Arcanjo <rafael.souza@yolo.com>
2026-01-15 12:56:12 +0200

    Fix spacing and formatting in build_cache function

--- lib/meme/application.ex
+++ lib/meme/application.ex
@@ -20,11 +20,11 @@ defmodule Meme.Application do
   end
 
   defp build_cache do
-    cachex_version = Application.spec(:cachex, :vsn) 
+    cachex_version = Application.spec(:cachex, :vsn)
     opts = [name: :meme]
 
     extra_opts =
-      if Version.match?(cachex_version, "~> 4.0")do
+      if Version.match?(cachex_version, "~> 4.0") do
         import Cachex.Spec, only: [router: 1]
 
         [
